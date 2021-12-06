d14f97598f7cc434557ee1e1cef202a0df020546
Erlend <erlend.edvardsen.vige@nav.no>
2021-12-06 14:49:42 +0100

    Set up Prometheus

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const { Storage } = require('@google-cloud/storage');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const { Storage } = require('@google-cloud/storage');
+const promClient = require('prom-client');
 
 const storage = new Storage();
 const bucket = storage.bucket('frontendplattform-assets-dev');
@@ -10,12 +11,25 @@ const secondsInAYear = 31536000;
 const corsOptions = {
     origin: process.env.CORS_ALLOWED_DOMAIN,
     optionsSuccessStatus: 200,
-    methods: 'GET,HEAD'
+    methods: 'GET,HEAD',
 };
+
+promClient.collectDefaultMetrics();
+const requestCounter = new promClient.Counter({
+    name: 'request_count',
+    help: 'Number of requests',
+    labelNames: ['file']
+});
 
 const app = express();
 app.use(helmet());
 app.use(cors(corsOptions));
+
+app.get('/internal/metrics', async (req, res) => {
+    const metrics = await promClient.register.metrics();
+    res.set('Content-Type', promClient.register.contentType).send(metrics);
+});
+
 app.get('/isReady', (req, res) => res.sendStatus(200));
 app.get('/isAlive', (req, res) => res.sendStatus(200));
 
@@ -24,6 +38,7 @@ app.get('/react-17.esm.js', (req, res) => {
 });
 
 app.get('/react-17.0.2.esm.js', (req, res) => {
+    requestCounter.inc({ file: 'react' });
     res.setHeader('Content-Type', 'application/javascript');
     res.setHeader('Cache-Control', `max-age=${secondsInAYear}`);
     const reactPathname = 'react/17.0.2/esm/index.js';
@@ -41,6 +56,7 @@ app.get('/react-dom-17.esm.js', (req, res) => {
 });
 
 app.get('/react-dom-17.0.2.esm.js', (req, res) => {
+    requestCounter.inc({ file: 'react-dom' });
     res.setHeader('Content-Type', 'application/javascript');
     res.setHeader('Cache-Control', `max-age=${secondsInAYear}`);
     const reactDomPathname = 'react-dom/17.0.2/esm/index.js';
