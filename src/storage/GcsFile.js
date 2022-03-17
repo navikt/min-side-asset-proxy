852863474cc1ccd87bd2fee6276ac62d269146c2
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-03-17 13:12:46 +0100

    Legger til debug logging av buckets

--- src/storage/GcsFile.js
+++ src/storage/GcsFile.js
@@ -3,6 +3,7 @@ const isProduction = require('../utils/isProduction');
 const storage = new Storage();
 
 const bucket = storage.bucket(isProduction ? 'min-side-assets-prod' : 'min-side-assets');
+const bucketName = isProduction ? 'min-side-assets-prod' : 'min-side-assets';
 
 class GcsFile {
     file = null;
@@ -10,6 +11,8 @@ class GcsFile {
 
     constructor(pathToFile) {
         this.file = bucket.file(pathToFile);
+        console.log(bucketName);
+        console.log(bucket);
         this.readStream = this.file.createReadStream().on('error', (error) => {
             console.error('Error reading file from bucket');
         });
