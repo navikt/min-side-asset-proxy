26f8e0d028b5011679321f2fb10b384071bc413f
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-06-02 11:32:14 +0200

    Bruk compression

--- src/middleware/applyDefaultMiddlewares.js
+++ src/middleware/applyDefaultMiddlewares.js
@@ -1,5 +1,6 @@
 const helmet = require('helmet');
 const cors = require('cors');
+const compression = require('compression');
 
 const corsOptions = {
     origin: process.env.CORS_ALLOWED_DOMAIN,
@@ -10,4 +11,5 @@ const corsOptions = {
 module.exports = (app) => {
     app.use(helmet());
     app.use(cors(corsOptions));
+    app.use(compression());
 };
